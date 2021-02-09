976546f687171d743f131050bad450ed4008d9ba
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-09 21:50:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
 var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  newKittens = [name, ...kittens];
+  return newKittens;
+}
+
+function prependKitten(name) {
+  newKittens = [...kittens, name];
+  return newKittens;
+}
+
+function removeLastKitten() {
+  newKittens = kittens.slice(0, kittens.length - 1);
+  return newKittens;
+}
+
+function removeFirstKitten() {
+  newKittens = kittens.slice(1);
+  return newKittens;
+}
